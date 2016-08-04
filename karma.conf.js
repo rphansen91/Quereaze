e64636fdf12264eef4336ba8ca727a4948aa3906
rphansen91 <rphansen91@gmail.com>
2016-08-04 15:47:05 -0700

    test(devops): Setup testsuite, code coverage, travis config, and semantic release
    closes #1

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+var webpackConfig = require('./webpack.config');
+
+webpackConfig.module.preLoaders = [
+  // instrument only testing sources with Istanbul
+  {
+    test: /\.ts$/,
+    loader: 'istanbul-instrumenter!ts'
+  }
+];
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      './node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
+      'src/**/*.test.js'
+    ],
+    exclude: [],
+    preprocessors: {
+      'src/**/*.ts': ['coverage'],
+      'src/**/*.test.js': ['webpack']
+    },
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve
+    },
+    reporters: ['progress','coverage'],
+    coverageReporter: {
+      type: 'json',
+      dir: 'reports',
+      subdir: 'coverage',
+      file: 'coverage-final.json'
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
